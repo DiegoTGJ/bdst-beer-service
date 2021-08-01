219ca5f3d147f6a3db10ec35cfcafd8b86002dd6
Diego Tapia Guajardo <practicadtapia@transbank.cl>
2021-08-01 14:58:10 -0400

    Add initial project. Closes #1

---
+++ src/main/java/diego/spring/msvc/bdstbeerservice/BdstBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package diego.spring.msvc.bdstbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdstBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdstBeerServiceApplication.class, args);
+    }
+
+}
